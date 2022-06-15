3e253888d1a81d1a14e6af53fb1d92cb0b9ba744
Hepii-x <mrhappyy14@gmail.com>
2022-06-15 10:55:49 +0200

    second commit

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from pytube import YouTube
 
 file1 = open('myfile.txt', 'r')
 Lines = file1.readlines()
-  
-# Strips the newline character
+
 for line in Lines:
     yt = YouTube(line)
     stream = yt.streams.get_audio_only()
